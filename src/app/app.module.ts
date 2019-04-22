c0b094b3daedb602ed6d7aaadaad5deef869f6ad
Pierre-Philippe Berenguer <Pepe@MacBook-Pro-de-Pierre-Philippe.local>
2019-04-22 20:54:59 +0200

    Ameliorations visuel chart graphique utilisation angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { GridComponent } from './grid/grid.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
